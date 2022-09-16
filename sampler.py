38feae19186bdcc3a6a1146fd62d9b118779c2a0
Giulia Avato <giulia@MacBook-Air-di-Giulia.local>
2022-09-16 19:19:33 +0200

    first commit

---
+++ sampler.py
@@ -0,0 +1,35 @@
+import numpy as np
+__author__ = 'Giulia Avato'
+__email__ = 'giulia.avato@studio.unibo.it'
+__version__ = '0.0.1'
+
+
+
+
+def main():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+	main()
+
