ec761a458291752d743d9f914fca5001348130fe
Giulia Avato <giulia@MacBook-Air-di-Giulia.local>
2022-09-22 21:54:54 +0200

    commit 6

--- sampler.py
+++ sampler.py
@@ -1,6 +1,5 @@
 import numpy as np
-from analytic_model import fakedata 
-
+from analytic_model import fakedata,model
 
 __author__ = 'Giulia Avato'
 __email__ = 'giulia.avato@studio.unibo.it'
@@ -18,13 +17,38 @@ def main():
 	data=fakedata(start,end,n,params,sigma)
 
 
+def likelihood(data,sigma=1,xn,params):
+
+	L = np.exp(-0.5*(np.log(2*np.pi*sigma**2) + 0.5*(data - model(x, params))**2 / sigma**2 )  )
+	#verosimiglianza #
+	return L  
 
 
-MCMC(start=0,end=1000,inf=0,sup=1)
-	"""This function samples a random number undergo a random walk 
-	through the posterior distribution where the probability of 
-	moving to any given point is determined by the transition
-	of the Makov chain."""
+def prior(xn,inf,sup): 
+	#sarà uniforme e normalizzata#
+	
+	if inf <= xn <= sup:
+		h=1/(sup-inf)
+	else:
+		h=0
+		
+	return h
+	 
+
+
+def prob(data,sigma,xn,params,inf,sup):
+	
+	p=likelihood(data,sigma,xn,params)*prior(xn,inf,sup)
+	
+	return p
+
+
+
+
+
+def MCMC(sigma,data, start=0,end=1000,inf=0,sup=1) : 
+	"""This function samples a random number, which steps resemble
+	a random walk."""
 
 	"""
 	xn : starting position
@@ -33,22 +57,23 @@ MCMC(start=0,end=1000,inf=0,sup=1)
 	U : uniform random number 
 	"""
  
-sigma=1
-xn = start
+	sigma=1	
+	xn = start
 
-for i in range (end):
+	for i in range (end):
 	
-	xn1= np.random.normal(xn,sigma)
-	controll =  prob(xn1,inf,sup)/prob(xn,inf,sup)
+		xn1= np.random.normal(xn,sigma)
+		controll =  prob(data,sigma,xn1,params,inf,sup)/prob(data,sigma,xn,params,inf,sup)
 
-	if controll>=1 :
-		xn1_acc=xn1
-	else :
-		U = np.random.rand(1,1)
+		if controll>=1 :
+			xn=xn1
+		else :
+			U = np.random.rand(1,1)
 		
-		if U<=controll
-			xn1_acc=xn
-		
+			if U<=controll
+				xn=xn1
+				#else xn=xn#
+	return xn		
 
 
 
