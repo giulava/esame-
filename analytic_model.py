8056cd3c83b7d6e2f26f84f7637845c77134b215
giulava <113693199+giulava@users.noreply.github.com>
2022-09-28 22:12:33 +0200

    last commit

--- analytic_model.py
+++ analytic_model.py
@@ -2,7 +2,7 @@ import numpy as np
 from numba import njit
 
 @njit
-def model(x,params):
+def model(x, params):
 	
 	"""
 	This function computes the analytic model that we will try to recover. 
@@ -15,26 +15,26 @@ def model(x,params):
 
 
 
-def noise(mu,sigma):
+def noise(mu, sigma):
 	"""
 	This function computes the noise to be superimposed to the model. 
 	Sigma is the standard deviation and mu the mean of the gaussian.
 	"""
 
-	return np.random.normal(mu,sigma) 
+	return np.random.normal(mu, sigma) 
 	  
 
 
 
-def fakedata(start,end,n,params,sigma):
+def fakedata(start, end, n, params, sigma):
 	"""
 	This function computes the experimental data points.
 	Start is the beginning of function domain, end his end, n is the number of points.
 	It gives as output an array containing the data points.
 	"""
 	
-	grid=np.linspace(start,end,n)
-	data=model(grid,params)
-	data=noise(data,sigma)
+	grid=np.linspace(start, end, n)
+	data=model(grid, params)
+	data=noise(data, sigma)
 	
-	return data,grid
+	return data, grid
