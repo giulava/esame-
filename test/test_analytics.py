4b73dfa51a11c958ab9311f967d90f8ba0232ab6
giulava <113693199+giulava@users.noreply.github.com>
2022-09-29 07:51:29 +0200

    last commit

--- test/test_analytics.py
+++ test/test_analytics.py
@@ -13,7 +13,7 @@ def test_model():
 	a=1
 	b=1
 	c=0
-	modelv=np.array([0,2,6,12])
+	modelv=np.array([0.5,1.5,3.5,6.5])
 		#Modelv contains exact values#
 	x=np.array([0,1,2,3])
 	modelt=model(x,a,b,c)
