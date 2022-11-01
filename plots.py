30b6792223ebe03f8bc2349cc5953cd936316265
giulava <113693199+giulava@users.noreply.github.com>
2022-11-01 14:48:16 +0100

    split plots from main function

---
+++ plots.py
@@ -0,0 +1,39 @@
+def makeplot(posteriorA,posteriorB,posteriorC,xstart,xend,grid,data):
+
+
+
+  plt.figure()
+	plt.hist(posteriorA, bins = 100, histtype='step', density = True, label='histogram') 
+	plt.show()
+
+	plt.figure()
+	plt.hist(posteriorB, bins = 100, histtype='step', density = True, label='histogram')
+	plt.show()
+
+	plt.figure()
+	plt.hist(posteriorC, bins = 100, histtype='step', density = True, label='histogram')
+	plt.show()
+	
+	q_16A,q_50A,q_84A = np.quantile(posteriorA, [0.16,0.5, 0.84], axis = 0)
+	q_mA, q_pA=q_50A-q_16A, q_84A-q_50A
+	print(f'{q_50A}  + {q_pA} - {q_mA}')
+
+
+	q_16B,q_50B,q_84B = np.quantile(posteriorB, [0.16,0.5, 0.84], axis = 0)
+	q_mB, q_pB=q_50B-q_16B, q_84B-q_50B
+	print(f'{q_50B}  + {q_pB} - {q_mB}')
+
+	q_16C,q_50C,q_84C = np.quantile(posteriorC, [0.16,0.5, 0.84], axis = 0)
+	q_mC, q_pC=q_50C-q_16C, q_84C-q_50C
+	print(f'{q_50C}  + {q_pC} - {q_mC}')
+
+	
+	eparams=np.array([q_50A,q_50B,q_50C])
+	xx=np.linspace(xstart,xend,1000)
+	fig=plt.figure()
+	plt.plot(grid,data,"o")
+	plt.plot(xx,model(xx,eparams))
+	plt.show()
+  
+  
+  
