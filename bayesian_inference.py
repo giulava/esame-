3271c23e43ef9c915a73e80d8e542764dffde4e3
giulava <113693199+giulava@users.noreply.github.com>
2022-11-01 15:31:55 +0100

    removal of configuration parameters

--- bayesian_inference.py
+++ bayesian_inference.py
@@ -1,7 +1,9 @@
 import numpy as np
+from config import *
 from sampler import MCMC
 from analytic_model import fakedata,model
 from plots import makeplot 
+
 
 
 __author__ = 'Giulia Avato'
@@ -10,18 +12,7 @@ __version__='1.0.0'
 
 
 def main():
-	"""Input
-	"""
-	init_MCMC=np.array([0.5,0.5,0.5], dtype = np.float64)
-	itmax=1000000
-	n=50
-	params=np.array([0.5,0.5,0.5], dtype = np.float64)
-	sigma=0.25
-	start=np.array([0,0,0], dtype = np.float64)
-	end=np.array([2,2,2], dtype = np.float64)
-	xstart=0
-	xend=10
-
+	
 	data,grid=fakedata(xstart,xend,n,params,sigma)
 	posterior=MCMC(sigma,data,grid,init_MCMC=init_MCMC,itmax=itmax,start=start,end=end)
 	posteriorA=posterior[:,0]
