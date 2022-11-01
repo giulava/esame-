894d7531a0c96408aa916f24e32f08770ac91d07
giulava <113693199+giulava@users.noreply.github.com>
2022-11-01 15:28:15 +0100

    create configuration file

---
+++ config.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+init_MCMC=np.array([0.5,0.5,0.5], dtype = np.float64)
+itmax=1000000
+n=50
+params=np.array([0.5,0.5,0.5], dtype = np.float64)
+sigma=0.25
+start=np.array([0,0,0], dtype = np.float64)
+end=np.array([2,2,2], dtype = np.float64)
+xstart=0
+xend=10
